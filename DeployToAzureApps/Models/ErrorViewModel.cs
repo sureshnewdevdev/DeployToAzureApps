60618116f295f8b2424ddf300385edd7c790d69e
sureshnewdevdev <45984902+sureshnewdevdev@users.noreply.github.com>
2023-07-17 00:44:59 +0530

    Add project files.

---
+++ DeployToAzureApps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeployToAzureApps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
